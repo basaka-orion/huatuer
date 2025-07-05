84a8d63e7355ed32171ed2288b24ddfbff3b2d5f
basaka-orion <162154986+basaka-orion@users.noreply.github.com>
2025-07-05 20:40:58 +0800

    Initial Commit

---
+++ HHTests/HHTests.swift
@@ -0,0 +1,16 @@
+//
+//  HHTests.swift
+//  HHTests
+//
+//  Created by ooo on 2025/7/5.
+//
+
+import Testing
+
+struct HHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
